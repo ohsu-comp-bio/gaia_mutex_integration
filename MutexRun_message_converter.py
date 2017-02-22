a0baec32e83f4b159eb833b0e8a244c465c1371b
Hannah Manning <manningh@ohsu.edu>
2017-02-22 11:59:04 -0800

    MutexRun_message_converter.py now invokes mutex

--- MutexRun_message_converter.py
+++ MutexRun_message_converter.py
@@ -1,49 +1,55 @@
 #!/usr/bin/env python
 
-# This script convertS MutexRun json messages into these files:
-#   parameters.txt  DataMatrix.txt
-# It will soon also invoke Mutex
+# This script converts MutexRun json messages into these files:
+#     parameters.txt  DataMatrix.txt
+# And subsequently invokes mutex using those files as input
+# It must be run from the file containing mutex.jar, MutexRun_pb2.py,
+#     resources directory containing PC2v8.sif, MutexRunMsg.txt
 
 # example bash command:
-# python MutexRun_message_converter.py -mrm MutexRunMsg_smalldevel.txt
-# mrm for mutex run message
+# python MutexRun_message_converter.py -mrm MutexRunMsg_DEVEL.txt
+# mrm is mutex run message
 
 import argparse
 import MutexRun_pb2
 import google.protobuf.json_format
+import os
 import subprocess
 
 # allow use of command line arguments for input and output files
 parser = argparse.ArgumentParser()
 parser.add_argument("-mrm")
 args = parser.parse_args()
-
 mrm_in_file = args.mrm
 mrm_in_fh = open(mrm_in_file, "r")
 
+# separate parameters and data matrix messages
 json_msg_list = mrm_in_fh.readlines()
-mrm_in_fh.close()
-
 param_msg = json_msg_list[0]
 mat_msg = json_msg_list[1]
+mrm_in_fh.close()
 
+# make mutex input directory (don't write over if already exists)
+os.makedirs("sample-input",exist_ok=True)
+
+# convert parameters json messages to mutex input format
 pm = google.protobuf.json_format.Parse(param_msg, MutexRun_pb2.Params())
-
 params = []
 
 for i in range(0,len(pm.paramnames)):
     params.append("{} {} {}{}".format(pm.paramnames[i], '=', pm.specifications[i], '\n'))
 
 params[-1] = params[-1].strip('\n')
-param_out_fh = open("parameters.txt", "w")
+
+# write parameters out to file
+param_out_fh = open("sample-input/parameters.txt", "w")
 
 for line in params:
     param_out_fh.write(line)
 
 param_out_fh.close()
 
-#matrix conversion below
-
+# convert data matrix json messages to mutex input format
 dmm = google.protobuf.json_format.Parse(mat_msg, MutexRun_pb2.Matrix())
 
 headerline = '\t'.join(dmm.header)+'\n'
@@ -56,7 +62,8 @@ for i in range(0,len(dmm.rows)):
     if i == len(dmm.rows)-1:
         rowlist[i] = rowlist[i].strip('\n')
 
-matrix_out_fh = open("DataMatrix.txt", "w")
+# write data matrix out to file
+matrix_out_fh = open("sample-input/DataMatrix.txt", "w")
 matrix_out_fh.write(headerline)
 
 for item in rowlist:
@@ -64,5 +71,7 @@ for item in rowlist:
 
 matrix_out_fh.close()
 
-#have to move a jarfile into working dir first
-#subprocess.run(["java", "-jar", "mutex.jar", "./"])
+# invoke mutex
+subprocess.run(["java", "-jar", "mutex.jar", "./sample-input"])
+# this would run the command:
+#     $ java -jar mutex.jar sample-input
