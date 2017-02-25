f827359584e3686b6d5593fbbc886f5a2ecfb2a3
Hannah Manning <manningh@ohsu.edu>
2017-02-24 16:01:02 -0800

    Compartmentalized MRM_Maker.py into functions

--- src/MRM_Maker.py
+++ src/MRM_Maker.py
@@ -21,58 +21,60 @@ m_infile = args.inmat
 p_infile = args.inparam
 outfile = args.outf
 
-p_infh = open(p_infile, "r")
-
 #initialize protobuf objects
 dmv = MR_pb2.Vector()
 dmm = MR_pb2.Matrix()
 mrm = MR_pb2.MutexRun()
 
-#assign the parameters to protobuf
-for line in p_infh:
-    line = line.strip('\n')
-    parts = line.split(' = ')
-    mrm.parameters[parts[0]]=parts[1]
+def assign_param_to_pb(pinfl, mrmessage):
+    p_infh = open(pinfl, "r")
+    for line in p_infh:
+        line = line.strip('\n')
+        parts = line.split(' = ')
+        mrmessage.parameters[parts[0]]=parts[1]
+    p_infh.close()
+    return mrmessage
 
-p_infh.close()
+def assign_matrix_to_pb(minfl,dmvector,dmmatrix):
+    m_infh = open(minfl, "r")
 
-#assign the matrix to protobuf
-m_infh = open(m_infile, "r")
+    # determine number of rows in matrix file
+    nlines = sum(1 for line in m_infh) - 1
+    m_infh.seek(0)
 
-# determine number of rows in matrix file
-# len(m_in_fh) does not work
-nlines = sum(1 for line in m_infh) - 1
-m_infh.seek(0)
+    #initialize counters and empty list of known size
+    count = 0
+    dmv_list = [None] * nlines
 
-#initialize counters and empty list of known size
-count = 0
-dmv_list = [None] * nlines
+    # parse matrix file and begin protobuf assignment
+    # i is a row in matrix
+    for line in m_infh:
+        line = line.strip('\n')
+        parts = line.split('\t')
+        if count == 0:
+            dmmatrix.header.extend(parts)
+        if count > 0:
+            dmvector.label = parts[0]
+            intcount = 1
+            for v in parts[1:]:
+                parts[intcount] = int(v)
+                intcount += 1
+            dmvector.values.extend(parts[1:])
+            dmv_list[count-1] = dmvector
+            dmvector = MR_pb2.Vector()
+        count += 1
+    dmmatrix.rows.extend(dmv_list)
+    m_infh.close()
+    return dmmatrix
 
-# parse matrix file and begin protobuf assignment
-# i is a row in matrix
-for line in m_infh:
-    line = line.strip('\n')
-    parts = line.split('\t')
-    if count == 0:
-        dmm.header.extend(parts)
-    if count > 0:
-        dmv.label = parts[0]
-        intcount = 1
-        for v in parts[1:]:
-            parts[intcount] = int(v)
-            intcount += 1
-        dmv.values.extend(parts[1:])
-        dmv_list[count-1] = dmv
-        dmv = MR_pb2.Vector()
-    count += 1
-dmm.rows.extend(dmv_list)
+def assign_all_to_mrm(outfl,mrmessage,dmmatrix):
+    mrmessage.matrix.MergeFrom(dmmatrix)
+    mrm_json = google.protobuf.json_format.MessageToJson(mrmessage).replace('\n', '')
+    outfh = open(outfl, "w")
+    outfh.write(mrm_json)
+    outfh.close()
 
-m_infh.close()
-
-#now all we have left to assign is mrm.matrix
-mrm.matrix.MergeFrom(dmm)
-mrm_json = google.protobuf.json_format.MessageToJson(mrm).replace('\n', '')
-
-outfh = open(outfile, "w")
-outfh.write(mrm_json)
-outfh.close()
+if __name__ = '__main__':
+    mrm = assign_param_to_pb(p_infile,mrm)
+    dmm = assign_matrix_to_pb(m_infile,dmv,dmm)
+    assign_all_to_mrm(outfile,mrm,dmm)
